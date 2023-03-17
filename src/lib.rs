5168acadb8108ae1b43981060299c35b3c0113cc
Laura María Alvarez Manrique <lalvar16@eafit.edu.co>
2023-03-17 01:59:40 -0500

    tests commit
    test commit

--- src/lib.rs
+++ src/lib.rs
@@ -10,44 +10,28 @@ pub fn mul(left: usize, right: usize) -> usize {
 pub fn div(left: usize, right: usize) -> usize {
     left / right
 }
-
 #[cfg(test)]
-mod tests_add {
+mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn it_works_sum() {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
-    
-}
 
-#[cfg(test)]
-mod tests_sub {
-    use super::*;
-#[test]
-    fn it_works() {
+    #[test]
+    fn it_works_sub() {
         let result = sub(2, 2);
         assert_eq!(result, 0);
     }
-}
-
-#[cfg(test)]
-mod tests_multiplication {
-    use super::*;
-#[test]
-    fn it_works() {
+    #[test]
+    fn it_works_mul() {
         let result = mul(2, 2);
-        assert_eq!(result, 3);
+        assert_eq!(result, 4);
     }
-}
-
-#[cfg(test)]
-mod tests_division {
-    use super::*;
-#[test]
-    fn it_works() {
+    #[test]
+    fn it_works_div() {
         let result = div(2, 2);
         assert_eq!(result, 1);
     }
