6bc9feab3d4ee7e0954501c7ef1ed59e3a550028
Laura María Alvarez Manrique <lalvar16@eafit.edu.co>
2023-03-17 00:39:59 -0500

    tests

---
+++ adder/src/lib.rs
@@ -0,0 +1,54 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+pub fn sub(left: usize, right: usize) -> usize {
+    left - right
+}
+pub fn mul(left: usize, right: usize) -> usize {
+    left * right
+}
+pub fn div(left: usize, right: usize) -> usize {
+    left / right
+}
+
+#[cfg(test)]
+mod tests_add {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+    
+}
+
+#[cfg(test)]
+mod tests_sub {
+    use super::*;
+#[test]
+    fn it_works() {
+        let result = sub(2, 2);
+        assert_eq!(result, 0);
+    }
+}
+
+#[cfg(test)]
+mod tests_multiplication {
+    use super::*;
+#[test]
+    fn it_works() {
+        let result = mul(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+#[cfg(test)]
+mod tests_division {
+    use super::*;
+#[test]
+    fn it_works() {
+        let result = div(2, 2);
+        assert_eq!(result, 1);
+    }
+}
---
+++ adder/target/debug/deps/adder-1c9091a267cd59c4.d
@@ -0,0 +1,7 @@
+/Users/lilarock3rs/Documents/VisualStudioProjects/rust/adder/target/debug/deps/adder-1c9091a267cd59c4.rmeta: src/lib.rs
+
+/Users/lilarock3rs/Documents/VisualStudioProjects/rust/adder/target/debug/deps/libadder-1c9091a267cd59c4.rlib: src/lib.rs
+
+/Users/lilarock3rs/Documents/VisualStudioProjects/rust/adder/target/debug/deps/adder-1c9091a267cd59c4.d: src/lib.rs
+
+src/lib.rs:
